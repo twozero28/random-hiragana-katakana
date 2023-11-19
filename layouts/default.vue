ce70f829c7740c4cef07dd05b8e970c730bbec4f
twozero28 <28twozero@gmail.com>
2023-11-19 15:55:45 +0900

    add github link

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,9 +3,18 @@
 </script>
 
 <template>
-<main class="w-screen h-screen flex items-center justify-center dark:bg-gray-900 dark:text-gray-100">
-  <slot/>
-</main>
+  <div class="w-screen h-screen flex flex-col dark:bg-gray-900 dark:text-gray-100">
+    <header class="w-full fixed">
+      <nav class="flex justify-end items-center p-2">
+        <a href="https://github.com/twozero28/random-hiragana-katakana" target="_blank">
+          <IconGithub class="w-6 h-6"/>
+        </a>
+      </nav>
+    </header>
+    <main class="w-full h-full flex items-center justify-center">
+      <slot/>
+    </main>
+  </div>
 </template>
 
 <style scoped>
